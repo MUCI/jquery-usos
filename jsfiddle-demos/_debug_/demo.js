1304f2990e33344960949d4b703ff3375a6d6ed9
Wojciech Rygielski <rygielski@mimuw.edu.pl>
2013-11-05 12:03:44 +0100

    Fix _debug_ demo.

--- jsfiddle-demos/_debug_/demo.js
+++ jsfiddle-demos/_debug_/demo.js
@@ -9,15 +9,13 @@ $(function() {
 
 	$(function() {
 		$.usosCore.usosapiFetch({
-			method: "services/apiref/method_index",
-			success: function(list) {
-				$("#result").text(list.length + " methods.");
-			},
-			fail: function() {
-				/* Cross-origin requests in usosapiProxy.php will work only
-				 * if both DEBUG flag and $debugUser field are set! */
-				$("#result").text("Failed to access usosapiProxy.php. Is $debugUser set?");
-			}
+			method: "services/apiref/method_index"
+		}).done(function(list) {
+			$("#result").text(list.length + " methods.");
+		}).fail(function() {
+			/* Cross-origin requests in usosapiProxy.php will work only
+			 * if both DEBUG flag and $debugUser field are set! */
+			$("#result").text("Failed to access usosapiProxy.php. Is $debugUser set?");
 		});
 	});
 });
